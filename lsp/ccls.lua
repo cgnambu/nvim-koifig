098f11b7787297751ca9293f95aefa06e204dbe8
Corey Nambu <corey.nambu@gmail.com>
2025-07-30 20:22:17 -0700

    Added lua_ls and ccls
    Also added lspconfig and lazy lua_ls configuration

---
+++ lsp/ccls.lua
@@ -0,0 +1,11 @@
+vim.lsp.config("ccls", {
+    init_options = {
+        compilationDatabaseDirectory = "build";
+        index = {
+            threads = 0;
+        };
+        clang = {
+            excludeArgs = { "-frounding-math"} ;
+        };
+    }
+})
---
+++ lua/core/lsp-config.lua
@@ -0,0 +1,18 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "folke/lazydev.nvim",       -- This gives us a better config for lua when editing nvim configurations
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
+    config = function()
+        vim.lsp.enable('lua_ls')
+        vim.lsp.enable('ccls')
+    end,
+}
