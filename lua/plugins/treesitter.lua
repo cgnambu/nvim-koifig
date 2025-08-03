b0c53b17b8799e56b9a8e4fb90402beacf402de3
Corey Nambu <corey.nambu@gmail.com>
2025-08-03 12:11:15 -0700

    Added cmake tools

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
     branch = 'master',
-    lazy = false, 
+    lazy = false,
     build = ":TSUpdate",
     config = function()
         local configs = require("nvim-treesitter.configs")
